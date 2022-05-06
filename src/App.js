9ba61247f203bd59af746fb771b8ae83bc55a1ca
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-06 16:13:00 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './scss/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App start</h1>
+        <h2>Begin</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
