f1704e359f0092da3c7c39f219e7c3d9694bacda
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-09 21:12:27 +0200

    add class to nav

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <div className='baner'><div className='img'></div> </div>
       <header className="App-header">
-        <a className='main' href='#'>O mnie</a>
-        <a href='#'>Multimedia i animacje</a>
-        <a href='#'>Grafika cyfrowa</a>
-        <a href='#'>Projektowanie graficzne</a>
-        <a href='#'>Identyfikacja wizualna</a>
-        <a href='#'>Ilustracje</a>
-        <a href='#'>Grafika 3D</a>
-        <a href='#'>Rysunki i szkice</a>
+        <a className='header-section' href='#'>O mnie</a>
+        <a className='header-section' href='#'>Multimedia i animacje</a>
+        <a className='header-section' href='#'>Grafika cyfrowa</a>
+        <a className='header-section' href='#'>Projektowanie graficzne</a>
+        <a className='header-section' href='#'>Identyfikacja wizualna</a>
+        <a className='header-section' href='#'>Ilustracje</a>
+        <a className='header-section' href='#'>Grafika 3D</a>
+        <a className='header-section' href='#'>Rysunki i szkice</a>
       </header>
     </div>
   );
