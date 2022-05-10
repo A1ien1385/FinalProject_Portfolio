ecee9b3d7937d736a95223574bcf3c92121e9d6a
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-10 21:26:00 +0200

    slider finish

--- src/slider.js
+++ src/slider.js
@@ -2,17 +2,14 @@ import "./scss/main.scss";
 
 function Slider() {
     
-
-    
-     return (
-         <>
+   return (
+         
         <div className='slider_box'>
             <div className='slide slide_one'></div>
             <div className='slide slide_two'></div>
             <div className='slide slide_three'></div>
         </div>
-        <h3>Tekst</h3>
-        </>
+        
      );
   }
   
