0b70de3e8a027db15ddeadd03418cd9ccdbc7784
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-10 14:26:45 +0200

    aboutMe end

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -3,7 +3,7 @@ function AboutMe() {
       <div className="about_author">
           <div className="about_author__photo"></div>
           <div className="about_author__text">
-              <h1 className='about_author__title'>Title</h1>
+              <h1 className='about_author__title'>O mnie</h1>
               <article className="about_author__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus odio, volutpat ac luctus vitae, lobortis non ex. Duis sit amet neque enim. Ut sit amet tempor elit. Morbi sed augue non dui sollicitudin dignissim ac ullamcorper sapien. Aenean vulputate, massa eget hendrerit pellentesque, sapien mi porttitor quam, sit amet pretium tellus orci id mauris. Fusce euismod cursus purus vel ultrices. Nam tincidunt ligula eget ultricies feugiat. Integer pulvinar elit non mauris rutrum interdum. Duis facilisis fringilla nisi, dapibus aliquam odio euismod sed.
                Vestibulum vulputate velit vitae pharetra egestas. Fusce massa sem, congue vel nunc a, malesuada iaculis ligula. Curabitur nibh est, ullamcorper id leo vitae, finibus porta est. Phasellus ac massa velit. Cras tortor sapien, lobortis sed ante ac, tincidunt accumsan sapien. In sed mi arcu. Ut sit amet felis at velit pellentesque consequat. Curabitur scelerisque convallis aliquam.
               </article> 
