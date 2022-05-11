eb24ef9887cd47ed6d8eea23d202d42b713c8382
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-11 14:15:17 +0200

    fixedMenu work

--- src/fixedMenu.js
+++ src/fixedMenu.js
@@ -2,24 +2,44 @@ import { useState } from "react";
 import "./scss/main.scss";
 
 export function FixedMenu() {
-// const widthhh = [widthhh, setWidth] = useState(50)
+const [state, setState] = useState(false);   
+const [width, setWidth] = useState(-100);
+const [txt, setTxt] = useState("Wysuń menu");
 
-const szerokosc = 50;
+function handleClickFixedMenu()
+{
+    if(state === false) 
+    {
+    setWidth(width => width + 100);
+    setTxt("Schowaj menu");
+    setState(true);
+    }
+
+    else 
+    {
+        setWidth(0);
+        setTxt("Wysuń menu");
+        setState(false);
+    }
+}
 const menu =
 {
+    display: "flex",
     position: "fixed",
     top: "0",
-    left: "50",
+    left: `${width}%`,
     backgroundColor: "red",
-    width: `${szerokosc}%`,
+    width: `100%`,
+    height: "100px",
+
 }
 
     return (
           <>
-         <div style={menu} className='fixed_menu_box menu_box_active'>
-            <h1>xxdddddd</h1>
+         <div style={menu}>
+             <p style={{position: "absolute", top: "10px", left: "10px", color: "white"}}>xdd</p>
          </div>
-         <button style={{position: "fixed", left: "0", top: "10%"}}>Wysuń menu</button>
+         <button onClick={handleClickFixedMenu} style={{position: "fixed", left: "0", top: "10%"}}>{txt}</button>
          </>
       );
    }
