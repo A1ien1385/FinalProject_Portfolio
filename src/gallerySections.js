a3d397644b0b0f8f8526b868eba5ab8797a2ce14
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-13 14:44:38 +0200

    sections art popup 2

--- src/gallerySections.js
+++ src/gallerySections.js
@@ -15,6 +15,7 @@ const toggleClass = () =>
           <h1 id="digital_art" className="art_sections_title">Grafika cyfrowa</h1>
           <section style={{backgroundImage: "url('https://drive.google.com/uc?id=1aeGaTZoetIyARLay9b46o4NrtiWivVrA')"}} onClick={toggleClass} className={isActive ? 'activeArt' : null}></section>
           <section style={{backgroundImage: "url('https://drive.google.com/uc?id=13HdAe5S6CaJvwq7Zlntqqf6X12997oCm')"}} onClick={toggleClass} className={isActive ? 'activeArt' : null}></section>
+          <section style={{backgroundImage: "url('https://drive.google.com/uc?id=1k7ZnzwBkz_5CRdQh0ctbol6QnAPlr0UP')"}} onClick={toggleClass} className={isActive ? 'activeArt' : null}></section>
          </>
       );
    }
