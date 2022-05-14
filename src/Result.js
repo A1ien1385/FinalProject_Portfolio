7b672be540a81901080bc6d8c00993c10c95af23
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-14 17:00:35 +0200

    weather API done

--- src/Result.js
+++ src/Result.js
@@ -3,16 +3,30 @@ import React from 'react';
 
 const Result = (props) => {
 
-    const {date, city, sunrise, sunset, temp, pressure, wind, err} = props.weather
+    const {date, city, sunrise, sunset, temp, pressure, wind, err} = props.weather;
+
+    let content = null;
+
+    if(!err && city)
+    {
+        const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
+        const sunsetTime = new Date(sunset * 1000).toLocaleTimeString();
+        content = (
+            <div>
+           <h3>{`Search for city ${city}:`}</h3>
+           <h4>{`Temperature: ${temp}`}</h4>
+           <h4>{`Sunrise: ${sunriseTime}`}</h4>
+           <h4>{`Sunset: ${sunsetTime}`}</h4>
+           <h4>{`Pressure: ${pressure} hPa`}</h4>
+           <h4>{`Wind: ${wind} m/s`}</h4>
+            </div>
+        )
+    }
+
     return (
-        <>
-        <div>Weather for: {city}</div>
-        <div>Temperature: {temp}</div>
-        <div>Sunrise: {sunrise}</div>
-        <div>Sunset: {sunset}</div>
-        <div>Pressure: {pressure}</div>
-        <div>Wind: {wind}</div>
-        </>
+        <h3 className='weatherResult'>
+           {err ? `No city with this name` : content} 
+        </h3>
     )
 }
 
