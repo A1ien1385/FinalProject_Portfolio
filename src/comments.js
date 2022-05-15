01ea95b3723f8b131c89702b52b49126bac8cd43
A1ien1385 <damian.zawadzki@tlen.pl>
2022-05-15 16:10:31 +0200

    Comments components2

--- src/comments.js
+++ src/comments.js
@@ -1,16 +1,21 @@
+import React, { Component } from 'react'; 
 import './scss/main.scss';
 import CommAdd from "./commAdd"
 import CommList from './commList';
 
 
-function Comments() {
+
+class Comments extends Component {
+
+  render()
+  {  
   return (
     <>
      <h1 id="comments" className="art_sections_title">Komentarze</h1>
      <CommAdd/>
      <CommList/>
     </>
-  );
+  )};
 }
 
 export default Comments;
